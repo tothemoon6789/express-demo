315a9655a94603c860fdedd061c279efd05ab325
tothemoon6789 <pnlinh6789@gmail.com>
2023-05-04 10:54:28 +0700

    hi

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require("express")
-const app = express()
-const port = 3000
-app.get('/',function (req,res) {
-    res.send(('Hello wolrd, anh em nhes'))
-    
-})
-app.listen(port,() => {
-    console.log(`expamle app listening on port ${port}`) 
-})
+const app = require('express')();
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
